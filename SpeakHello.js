01a5dbc96dd3fa3695d25c8a8c8746ce1ae02829
vipul reddy <36426432+nallavipulreddy@users.noreply.github.com>
2020-04-20 23:52:30 +0530

    commit final

--- SpeakHello.js
+++ SpeakHello.js
@@ -17,11 +17,11 @@
 (function (window) {
 	var helloSpeaker = {};
 	var speakWord = "Hello";
-	helloSpeaker.speak= function(name) {
-  	console.log(speakWord + " " + name);
+	helloSpeaker.speak = function (name) {
+		console.log(speakWord + " " + name);
 	}
 	window.helloSpeaker = helloSpeaker;
-});(window);
+})(window);
 
 
 // STEP 5: Expose the 'helloSpeaker' object to the global scope. Name it
